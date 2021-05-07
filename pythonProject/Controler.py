adfd78b737b5af978e130f756aa746ee7fd42d4b
naegel23 <64175688+naegel23@users.noreply.github.com>
2021-05-07 18:34:17 +0200

    Update Controler.py

--- pythonProject/Controler.py
+++ pythonProject/Controler.py
@@ -1,25 +1,16 @@
 from Model import *
 from View import View
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 import sys
-
-
-
-db = TinyDB("db.json")
-players_db = db.table("players")
-tournaments_db = db.table("tournaments")
-# global_players = Players()
-# for row in players_db:
-#     global_players.add(
-#         Player(row["name"], row["firstname"],
-#                row["birthday"], row["gender"], row["rank"]))
 
 
 class Controller:
 
     def __init__(self):
         self.view = View()
-
+        db = TinyDB("db.json")
+        self.players_db = db.table("players")
+        self.tournaments_db = db.table("tournaments")
 
         # === START PROGRAM ===
     def start_program(self):
@@ -31,19 +22,13 @@ class Controller:
                 Controller.insert_player(self)
             elif choice == 2:
                 # Create tournament
-                Controller.init_tournament(self)
+                Controller.create_tournament(self)
             elif choice == 3:
-                #Add player to tournament
-                self.ask_which_player()
+                # Import tournament
+                Controller.choice_import_tournament(self, self.tournaments_db)
             # elif choice == 4:
-            #     # Import tournament
-            #     Controller.ask_which_tournament(self)
-            # # elif choice == 3:
-            # #     # Continue an existing tournament
-            # #     # Controller.import_tournament()
-            # elif choice == 5:
             #     Controller.play_rounds(self)
-            elif choice == 6:
+            elif choice == 4:
                 View.choice_leave_tournament()
                 return sys.exit()
             else:
@@ -64,9 +49,9 @@ class Controller:
         players = Players()
         newplayer = self.create_player()
         players.add(newplayer)
-        players_db.insert(newplayer.to_dict())
+        self.players_db.insert(newplayer.to_dict())
 
-    def init_tournament(self):
+    def init_tournament(self, players):
         name = self.view.tournament_name()
         location = self.view.tournament_location()
         # date = self.view.tournament_date()
@@ -74,84 +59,141 @@ class Controller:
         time = self.view.time_controler()
         description = self.view.get_description()
 
-        self.tournament = Tournament(name, location, rounds, time, description)
-        tournaments_db.insert(self.tournament.to_dict())
+        return Tournament(name, location, players, rounds, time, description)
 
-    # def add_player(self):
-    #     research_player = self.ask_which_player(players_db)
-    #     self.tournament.players.append(
-    #         Player(research_player[0]["name"], research_player[0]["first_name"], research_player[0]['birthday'],
-    #                research_player[0]["gender"], research_player[0]["rank"]))
-    #     for players in self.tournament.players:
-    #         print(players.to_dict())
-
-    def ask_which_player(self):
-        for (i, player) in enumerate(players_db):
-            View.display_player_firstname_lastname(str(i), player.name, player.first_name)
+    def create_tournament(self):
+        View.intro_choice_create_tournament()
+        for (i, player) in enumerate(self.players_db):
+            View.display_player_firstname_lastname(str(i), player["name"], player["first_name"])
 
         id_list = []
         for i in range(8):
-            id_list.append(View.add_player(id_list, len(players_db)))
+            id_list.append(View.add_player(id_list, len(self.players_db)))
         players = Players()
 
         for (i, element) in enumerate(id_list):
+            data = self.players_db.get(doc_id=element + 1)
             players.add(
-                Player(players.list[i].name,
-                       players.list[i].first_name,
-                       players.list[i].gender,
-                       players.list[i].birthdate,
-                       players.list[i].rank))
+                Player(data["name"],
+                       data["first_name"],
+                       data["gender"],
+                       data["birthday"],
+                       data["rank"]))
 
         View.intro_list_of_players()
         for (i, player) in enumerate(players.list):
             View.display_player_firstname_lastname(
-                str(i), player.firstname, player.lastname)
+                str(i), player.name, player.first_name)
             player.reset_score()
+        tournament = self.init_tournament(players)
+        print(tournament.to_dict())
+        self.tournaments_db.insert(tournament.to_dict())
+        View.intro_list_of_players()
+        Controller.play_rounds(self, tournament, players)
+
+        View.display_results(tournament)
+
+    def play_rounds(self, tournament, p):
+        for i in range(tournament.current_round(), int(tournament.rounds)):
+            if i == 0:
+                p.order_by_rank()
+                player_pairs = p.matchmaking_round1()
+            else:
+                p.order_by_score()
+                player_pairs = p.matchmaking_over_rounds(tournament)
+            for player in p.list:
+                View.display_player_infos(player.first_name, player.name, str(player.rank), str(player.score))
+
+            matches = Matches()
+            for pair in player_pairs:
+                white_player = 0
+                black_player = 1
+                matches.add_match(Match(pair[0], pair[1]))
+            View.print_matches(matches)
+
+            for match in matches.list:
+                match.winner = View.ask_winner(match)
+                if match.winner is None:
+                    match.white_player.draw_match()
+                    match.black_player.draw_match()
+                else:
+                    match.winner.won_match()
+
+            tournament.add_round(matches)
+            tournament_in_db = Query()
+            self.tournaments_db.update(tournament.to_dict(), tournament_in_db.id == tournament.id)
+
+            continue_choice = View.ask_to_continue()
+            if continue_choice == 0:
+                return
+
+    def choice_import_tournament(self, tournaments_db):
+    #enlever tournaments-db et remplacer par self.tournaments_db
+        View.intro_choice_import_tournament()
+        result = self.ask_which_tournament()[0]
+
+        players = Players()
+        tournament = Tournament(result["name"], result["location"], players,
+                                result["rounds"],
+                                result["timer"], result["description"])
+        tournament.id = result["id"]
+
+        for player in result["players"]["list"]:
+            current_player = Player(player["name"], player["first_name"],
+                                    player["gender"], player["birthday"],
+                                    player["rank"])
+            current_player.score = player["score"]
+
+            players.add(current_player)
+
+        for rounds in result["round_list"]:
+            temp_round = Matches()
+            for matches in rounds["list"]:
+                white_player = Player(matches["white_player"]["name"],
+                                      matches["white_player"]["first_name"],
+                                      matches["white_player"]["gender"],
+                                      matches["white_player"]["birthday"],
+                                      matches["white_player"]["rank"])
+                white_player.score = matches["white_player"]["score"]
+                black_player = Player(matches["black_player"]["name"],
+                                      matches["black_player"]["first_name"],
+                                      matches["black_player"]["gender"],
+                                      matches["black_player"]["birthday"],
+                                      matches["black_player"]["rank"])
+                white_player.score = matches["black_player"]["score"]
+                temp_match = Match(white_player, black_player)
+                if matches["winner"] is not None:
+                    temp_match.set_winner(Player(
+                        matches["winner"]["name"],
+                        matches["winner"]["first_name"],
+                        matches["winner"]["gender"],
+                        matches["winner"]["birthday"],
+                        matches["winner"]["rank"]))
+                    temp_match.winner.score = matches["winner"]["score"]
+                temp_round.add_match(temp_match)
+
+            tournament.add_round(temp_round)
 
         View.intro_list_of_players()
-        # self.play_rounds()
-        # player_name = self.view.add_player(id_list, player_range)
-        # query = Query()
-        # result = players_db.search(query.name == player_name)
+        for (i, player) in enumerate(players.list):
+            View.display_player_firstname_lastname(
+                str(i), player.name, player.first_name)
+
+        Controller.play_rounds(self, tournament, players)
+        View.display_results(tournament)
+
+    def ask_which_tournament(self):
+        for tournament in self.tournaments_db.all():
+            View.display_tournament_infos(
+                str(tournament["id"]), tournament["name"],
+                tournament["location"])
+
+        tournament_id = View.ask_tournament_id(len(self.tournaments_db))
+        query = Query()
+        result = self.tournaments_db.search(query.id == tournament_id)
+
+        return result
 
 
-    # def play_rounds(self):
-    #     p = Players()
-    #     for i in range(self.tournament.current_round(), int(self.tournament.rounds)):
-    #             if i == 0:
-    #                 p.order_by_rank()
-    #                 player_pairs = p.matchmaking_round1()
-    #             else:
-    #                 p.order_by_score()
-    #                 player_pairs = p.matchmaking_over_rounds(self.tournament)
-    #             for player in p.list:
-    #                 View.display_player_infos(player.first_name, player.name,
-    #                                         str(player.rank), str(player.score))
-    #
-    #             matches = Matches()
-    #             for pair in player_pairs:
-    #                 white_player = 0
-    #                 black_player = 1
-    #                 matches.add_match(Match(pair[0], pair[1]))
-    #             View.print_matches(matches)
-    #
-    #             for match in matches.list:
-    #                 match.winner = View.ask_winner(match)
-    #                 if match.winner is None:
-    #                     match.white_player.draw_match()
-    #                     match.black_player.draw_match()
-    #                 else:
-    #                     match.winner.won_match()
-    #
-    #             self.tournament.add_round(matches)
-    #
-    #             continue_choice = View.ask_to_continue()
-    #             if continue_choice == 0:
-    #                 return
 controller = Controller()
 controller.start_program()
-controller.create_player()
-controller.insert_player()
-# controller.play_rounds()
-# controller.ask_which_tournament()
-# controller.create_tournament()
